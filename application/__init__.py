d8047a75e68723007ddb3716c791f4cabb592ce1
MustaphaYinka Yusuf <61212786+Mustaphayinka@users.noreply.github.com>
2021-12-15 23:14:14 +0100

    Add files via upload

---
+++ application/__init__.py
@@ -0,0 +1,24 @@
+from flask import Flask
+import pymongo
+
+
+app = Flask(__name__)
+# app.config['MONGO_URI'] = "mongodb+srv://test:password1234@cluster0.hx8ib.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"
+# Connecting to local mongodb database
+# client = pymongo.MongoClient('mongodb://127.0.0.1:27017/')
+# Creating a database
+
+
+# Online database
+client = pymongo.MongoClient("mongodb+srv://test:password1234@cluster0.hx8ib.mongodb.net/myFirstDatabase?retryWrites=true&w=majority")
+db = client.db
+
+
+# set up mongodb
+# madrasah = client['madrasah']
+# Creating a collection in the database already created
+# studentCollection = madrasah['students']
+
+
+
+from application import routes
